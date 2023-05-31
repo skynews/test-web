5028eefe0f4375421fde536f43672b6bf336395e
sky <sky@163.com>
2023-06-01 01:33:44 +0800

    新增division方法

--- js/math.js
+++ js/math.js
@@ -33,3 +33,14 @@ function minus(num1, num2) {
 function multi(num1, num2) {
   return num1 * num2;
 }
+
+/**
+ * 第4次 添加除法函数
+ * @param {*} num1 数字1
+ * @param {*} num2 数字2
+ * @returns 两数之商
+ */
+function division(num1, num2) {
+  console.log(num1, num2);
+  return num1 / num2;
+}
