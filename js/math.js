a07f9c8ca5e5a91c3527035768c4d272e0b71898
sky <sky@163.com>
2023-06-01 00:35:01 +0800

    第3次,添加乘法函数

--- js/math.js
+++ js/math.js
@@ -9,6 +9,8 @@
  * @returns 两数之和
  */
 function add(num1, num2) {
+  console.log(num1, num2);
+
   return num1 + num2;
 }
 
@@ -19,5 +21,19 @@ function add(num1, num2) {
  * @returns 两数之差
  */
 function minus(num1, num2) {
+  console.log(num1, num2);
+
   return num1 - num2;
+}
+
+/**
+ * 第3次 添加乘法函数
+ * @param {*} num1 数字1
+ * @param {*} num2 数字2
+ * @returns 两数乘积
+ */
+function minus(num1, num2) {
+  console.log(num1, num2);
+
+  return num1 * num2;
 }
