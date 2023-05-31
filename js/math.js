cc92e80293d579a2b5edfdd181fe46518148f83a
sky <sky@163.com>
2023-06-01 00:32:23 +0800

    第2次,添加minus函数

--- js/math.js
+++ js/math.js
@@ -11,3 +11,13 @@
 function add(num1, num2) {
   return num1 + num2;
 }
+
+/**
+ * 第2次 添加减法函数
+ * @param {*} num1 数字1
+ * @param {*} num2 数字2
+ * @returns 两数之差
+ */
+function minus(num1, num2) {
+  return num1 - num2;
+}
