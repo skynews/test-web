e07e5f7e28fe427a4be167d994fa4539f893224e
sky <sky@163.com>
2023-06-01 00:43:14 +0800

    新增log

--- js/math.js
+++ js/math.js
@@ -9,6 +9,7 @@
  * @returns 两数之和
  */
 function add(num1, num2) {
+  console.log(num1, num2);
   return num1 + num2;
 }
 
@@ -19,5 +20,6 @@ function add(num1, num2) {
  * @returns 两数之差
  */
 function minus(num1, num2) {
+  console.log(num1, num2);
   return num1 - num2;
 }
